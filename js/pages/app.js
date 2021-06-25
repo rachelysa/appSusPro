dba719e49815aedb10202c1938c85bd825f7d371
rachel vaikselbom <r45325098@gmail.com>
2021-06-25 13:10:31 +0300

    home page

--- js/pages/app.js
+++ js/pages/app.js
@@ -2,7 +2,24 @@ import appHeader from "../cmps/app-header.js"
 export default {
     template:`
         <section class="app-main">
-           hello
+           <img class="home-responsive" src="../../img/responsive.png"/>
+           <div class="go-to">
+           <div class="try-home"> 
+           
+             <div class="go-to-home email">
+                 <div class="go-to-icon"><i class="fas fa-envelope"></i></div>
+                 <h3>email</h3>
+                 <p>jbuodac kbuiubbpisckbou;</p>
+                  <button>go to email</button>
+                </div>
+             <div class="go-to-home notes">
+                 <div  class="go-to-icon"><i class="fas fa-sticky-note"></i></div>
+                 <h3>notes</h3>
+                 <p>jbuodac kbuiubbpisckbou;</p>
+                  <button>go to notes</button>
+                </div>
+           <div>
+         </div>
          
         </section>
     `,
