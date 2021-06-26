12142fe2d71eab1f591744ab821d44b42a8ae8e9
rachel vaikselbom <r45325098@gmail.com>
2021-06-26 22:20:28 +0300

    home changes

--- js/pages/app.js
+++ js/pages/app.js
@@ -1,6 +1,6 @@
 import appHeader from "../cmps/app-header.js"
 export default {
-    template:`
+  template: `
         <section class="home-main">
            <img class="home-responsive" src="./img/responsive.png"/>
            <div class="go-to">
@@ -9,24 +9,26 @@ export default {
              <div class="go-to-home email">
                  <div class="go-to-icon"><i class="fas fa-envelope"></i></div>
                  <h3>email</h3>
-                 <p>jbuodac kbuiubbpisckbou;</p>
-                  <button>go to email</button>
+                 <p>use email to send and get messeges</p>
+                  <button @click="goTo('/mail/inbox')">go to email</button>
                 </div>
              <div class="go-to-home notes">
                  <div  class="go-to-icon"><i class="fas fa-sticky-note"></i></div>
                  <h3>notes</h3>
-                 <p>jbuodac kbuiubbpisckbou;</p>
-                  <button>go to notes</button>
+                 <p>use notes to save all the stuff you want</p>
+                  <button @click="goTo('/note')" >go to notes</button>
                 </div>
 </div>
          </div>
          
         </section>
     `,
-      methods: {
-
-    },
-    components:{
-        appHeader
+  methods: {
+    goTo(url) {
+      this.$router.push(url)
     }
+  },
+  components: {
+    appHeader
+  }
 }
